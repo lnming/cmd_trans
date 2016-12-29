9cd1a191ca34f513e855babbd22cbf142f96958d
ginson.wang <kingsionwang@gmail.com>
2016-12-29 22:28:20 +0800

    改为用argparse管理参数

--- cmd_trans.py
+++ cmd_trans.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests,sys
+import requests,sys,argparse
 
 def translate(query):
-    params = {'keyfrom':'lin-trans','key':914995308,'version':1.1,'type':'data','doctype':'json','q':query}
-    r = requests.get('http://fanyi.youdao.com/openapi.do',params=params)
-    #print(r.json()['query'])
-    print(r.json()['translation'][0])
+	params = {'keyfrom':keyfrom,'key':key,'version':1.1,'type':'data','doctype':'json','q':query}
+	r = requests.get('http://fanyi.youdao.com/openapi.do',params=params)
+	#print(r.json()['query'])
+	print(r.json()['translation'][0])
 
-def trans(query=None):
-    if query  == None:
-        argv = sys.argv
-        if len(argv) == 1 and __name__ == '__main__':
-            print('命令行翻译工具\n用法：trans 要翻译的单词或句子')
-        elif len(argv) == 1 and __name__ != '__main__':
-            raise TypeError('请输入要翻译的单词或句子')
-        elif len(argv) > 1:
-            query = ' '.join(argv[1:])
-            translate(query)
-    else:
-        translate(query)
+key = 914995308
+keyfrom = 'lin-trans'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('query',help='输入需翻译的文本')
+args = parser.parse_args()
+QUERY = args.query
 
 if __name__ == '__main__':
-    trans()
+	translate(QUERY)
