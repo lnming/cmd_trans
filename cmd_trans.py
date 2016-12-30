489c2556fead79a3274ad8024cb9e3bd76a8e139
ginson <kingsionwang@gmail.com>
2016-12-30 17:58:02 +0800

    fix import as module bug

--- cmd_trans.py
+++ cmd_trans.py
@@ -3,19 +3,20 @@
 
 import requests,argparse
 
-def translate(query):
+key = 914995308
+keyfrom = 'lin-trans'
+
+def translate(query=''):
 	params = {'keyfrom':keyfrom,'key':key,'version':1.1,'type':'data','doctype':'json','q':query}
 	r = requests.get('http://fanyi.youdao.com/openapi.do',params=params)
 	#print(r.json()['query'])
 	print(r.json()['translation'][0])
 
-key = 914995308
-keyfrom = 'lin-trans'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('query',nargs='+',help='输入需翻译的文本')
-args = parser.parse_args()
-QUERY = ' '.join(args.query)
-
-if __name__ == '__main__':
+if __name__ == '__main__':	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('query',nargs='+',help='输入需翻译的文本',type=str,default='')
+	args = parser.parse_args()
+	QUERY = ' '.join(args.query)
+	
 	translate(QUERY)
