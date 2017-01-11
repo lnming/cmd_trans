f57b9be10a75c681e696c3ffe141bc9e207deca5
ginson <kingsionwang@gmail.com>
2017-01-11 18:08:48 +0800

    结果增加翻译模式和字典模式(暂不支持命令行参数，argparse不会用)

--- cmd_trans.py
+++ cmd_trans.py
@@ -6,17 +6,28 @@ import requests,argparse
 key = 914995308
 keyfrom = 'lin-trans'
 
-def translate(query=''):
+def translate(query,mode='all'):
 	params = {'keyfrom':keyfrom,'key':key,'version':1.1,'type':'data','doctype':'json','q':query}
 	r = requests.get('http://fanyi.youdao.com/openapi.do',params=params)
-	#print(r.json()['query'])
-	print(r.json()['translation'][0])
+	translation = r.json()['translation'][0]
+	try:
+		explains = r.json()['basic']['explains']
+	except:
+		print(translation)
+	else:
+		if translation != explains[0]:
+			if mode == 'all':
+				print('%s\n%s' %(translation,'\n'.join(explains)))
+			elif mode == 'trans':
+				print(translation)
+			elif mode == 'dict':
+				print('\n'.join(explains))
+		else:
+			print(translation)
 
-
-if __name__ == '__main__':	
+if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('query',nargs='+',help='输入需翻译的文本',type=str,default='')
 	args = parser.parse_args()
 	QUERY = ' '.join(args.query)
-	
-	translate(QUERY)
+	translate(QUERY,'all')
